6f4e02b7ab4963dafbf76bb7378bf928171d3e65
Bjorn Krols <krols.bj@gmail.com>
2020-12-05 14:25:15 +0100

    Adds record name / type / value to output

--- README.md
+++ README.md
@@ -91,7 +91,7 @@ locals {
 }
 
 module "route53_hosted_zone_certificate" {
-  source = "github.com/botre/terraform-recipes/modules/aws/route53-hosted-zone-certificate"
+  source = "github.com/botre/terraform-recipes/modules/aws/route-53-hosted-zone-certificate"
   hosted_zone_name = local.hosted_zone.name
   certificate_domain_name = local.certificate_domain_name
   certificate_alternate_domain_names = local.certificate_alternate_domain_names
--- modules/aws/route-53-hosted-zone-certificate/outputs.tf
+++ modules/aws/route-53-hosted-zone-certificate/outputs.tf
@@ -1,3 +1,16 @@
 output "certificate_domain_name" {
   value = aws_acm_certificate.certificate.domain_name
 }
+
+output "certificate_record_name" {
+  value = aws_route53_record.validation_record.name
+}
+
+output "certificate_record_type" {
+  value = aws_route53_record.validation_record.type
+}
+
+output "certificate_record_value" {
+  value = aws_acm_certificate.certificate.domain_validation_options[0].resource_record_value
+  sensitive = true
+}
