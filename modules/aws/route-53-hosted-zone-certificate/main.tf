54f63606f4596d6e79e733a6b085c3bfce1d6c46
Bjorn Krols <krols.bj@gmail.com>
2020-12-06 19:48:00 +0100

    Revert approachÃ

--- modules/aws/route-53-hosted-zone-certificate/main.tf
+++ modules/aws/route-53-hosted-zone-certificate/main.tf
@@ -6,25 +6,18 @@ resource "aws_acm_certificate" "certificate" {
 }
 
 resource "aws_route53_record" "validation_record" {
-  for_each = {
-    for dvo in aws_acm_certificate.certificate.domain_validation_options : dvo.domain_name => {
-      name = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type = dvo.resource_record_type
-      zone_id = data.aws_route53_zone.hosted_zone.zone_id
-    }
-  }
-
-  allow_overwrite = true
-  name = each.value.name
-  records = [each.value.record]
+  name = tolist(aws_acm_certificate.certificate.domain_validation_options)[0].resource_record_name
+  type = tolist(aws_acm_certificate.certificate.domain_validation_options)[0].resource_record_type
+  zone_id = data.aws_route53_zone.hosted_zone.zone_id
+  records = [
+    tolist(aws_acm_certificate.certificate.domain_validation_options)[0].resource_record_value]
   ttl = 60
-  type = each.value.type
-  zone_id = each.value.zone_id
 }
 
 resource "aws_acm_certificate_validation" "certificate_validation" {
   provider = aws.aws-us-east-1
   certificate_arn = aws_acm_certificate.certificate.arn
-  validation_record_fqdns = [for record in aws_route53_record.validation_record : record.fqdn]
+  validation_record_fqdns = [
+    aws_route53_record.validation_record.fqdn,
+  ]
 }
