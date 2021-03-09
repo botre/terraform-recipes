af88692b8460c073133d1412d8cbf01270b9257d
Bjorn Krols <krols.bj@gmail.com>
2021-03-09 07:58:19 +0100

    Expose s3_bucket_region

--- modules/aws/s3-cloudfront-website/outputs.tf
+++ modules/aws/s3-cloudfront-website/outputs.tf
@@ -2,6 +2,10 @@ output "s3_bucket_id" {
   value = aws_s3_bucket.bucket.id
 }
 
+output "s3_bucket_region" {
+  value = aws_s3_bucket.bucket.region
+}
+
 output "cloudfront_distribution_id" {
   value = aws_cloudfront_distribution.distribution.id
 }
