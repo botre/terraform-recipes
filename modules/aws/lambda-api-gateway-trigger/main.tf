c02173c08534b1978ed18c944c8fa811523f8574
Bjorn Krols <krols.bj@gmail.com>
2021-04-22 08:55:11 +0200

    Adds gateway_settings

--- modules/aws/lambda-api-gateway-trigger/main.tf
+++ modules/aws/lambda-api-gateway-trigger/main.tf
@@ -3,6 +3,16 @@ resource "aws_cloudwatch_log_group" "gateway_execution_log_group" {
   retention_in_days = 7
 }
 
+resource "aws_api_gateway_method_settings" "gateway_settings" {
+  rest_api_id = aws_api_gateway_rest_api.gateway_rest_api.id
+  stage_name = var.stage_name
+  method_path = "*/*"
+  settings {
+    metrics_enabled = true
+    logging_level = "ERROR"
+  }
+}
+
 resource "aws_api_gateway_rest_api" "gateway_rest_api" {
   name = "${data.aws_lambda_function.function.function_name}-api"
 }
