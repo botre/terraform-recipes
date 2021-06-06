1ca441a53a5da79615055ea2d8ccba1bb9835062
Bjorn Krols <krols.bj@gmail.com>
2021-06-06 13:17:53 +0200

    Re-add outputs

--- modules/aws/route-53-hosted-zone-certificate/outputs.tf
+++ modules/aws/route-53-hosted-zone-certificate/outputs.tf
@@ -1,3 +1,11 @@
 output "certificate" {
   value = aws_acm_certificate.certificate
 }
+
+output "certificate_arn" {
+  value = aws_acm_certificate.certificate.arn
+}
+
+output "certificate_id" {
+  value = aws_acm_certificate.certificate.id
+}
